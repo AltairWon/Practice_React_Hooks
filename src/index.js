720f5d0fbb2a12624d214a4c1ac0ecd5dbdb5710
Altair Won <wonhyokjun@gmail.com>
2021-03-12 10:11:21 +0900

    #1.2 useInput part Two

--- src/index.js
+++ src/index.js
@@ -5,24 +5,35 @@ import "./styles.css";
 
 //useInput: 기본적으로 input을 업데이트한다.
 //이벤트를 분리된 파일, 다른 entity에 연결해서 처리 할 수 있다.
-const useInput = (initialValue) => {
+const useInput = (initialValue, validator) => {
   const [value, setValue] = useState(initialValue);
   const onChange = (event) => {
-    console.log(event.target);
+    const {
+      target: { value }
+    } = event;
+    //willUpdat = true: 항상 업데이트가 될 것이다.
+    let willUpdate = true;
+    if (typeof validator === "function") {
+      willUpdate = validator(value);
+    }
+    if (willUpdate) {
+      setValue(value);
+    }
   };
   return { value, onChange };
 };
 
 //{...name}으로 간단하게 처리를 할 수 있다.
-function App() {
-  const name = useInput("Mr.");
+const App = () => {
+  const maxLen = (value) => !value.includes("@");
+  const name = useInput("Mr.", maxLen);
   return (
     <div className="App">
       <h1>Hello</h1>
       <input placeholder="Name" {...name} />
     </div>
   );
-}
+};
 
 const rootElement = document.getElementById("root");
 ReactDOM.render(
