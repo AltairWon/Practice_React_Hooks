3f12016a5782a524740740ab6217fb869e82e13c
Altair Won <wonhyokjun@gmail.com>
2021-03-12 12:16:49 +0900

    #2.1 useTitle

--- src/index.js
+++ src/index.js
@@ -3,20 +3,23 @@ import ReactDOM from "react-dom";
 
 import "./styles.css";
 
+//useTItle = title에 대한 update를 사용할 수 있다.
+const useTitle = (initialTitle) => {
+  const [title, setTitle] = useState(initialTitle);
+  const updateTitle = () => {
+    const htmlTitle = document.querySelector("title");
+    htmlTitle.innerText = title;
+  };
+  useEffect(updateTitle, [title]);
+  return setTitle;
+};
+
 const App = () => {
-  const sayHello = () => console.log("Hello");
-  //useEffect = componentDidmoud의 역할을 한다.
-  //새로고침을 하면 sayHello를 실행한다.
-  //componentDidUpdate의 역할도 하기 때문에 클릭하면 sayHello를 실행한다.
-  const [number, setNumber] = useState(0);
-  const [aNumber, setAnumber] = useState(0);
-  //첫번째는 function, 두번째는 조건
-  useEffect(sayHello, []);
+  const titleUpdater = useTitle("Loading...");
+  setTimeout(() => titleUpdater("Home"), 5000);
   return (
     <div className="App">
       <div>Hi</div>
-      <button onClick={() => setNumber(number + 1)}>{number}</button>
-      <button onClick={() => setAnumber(aNumber + 1)}>{aNumber}</button>
     </div>
   );
 };
